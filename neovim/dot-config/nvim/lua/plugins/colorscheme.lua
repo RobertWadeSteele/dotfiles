6b84d09f7218f60f8a1f376aadf9058cc33ad3fb
RobertWadeSteele <robert.wade.steele@gmail.com>
2025-01-19 16:27:12 -0500

    revert color scheme change
    Tokyonight is just the best.

--- neovim/dot-config/nvim/lua/plugins/colorscheme.lua
+++ neovim/dot-config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,11 @@
 ---@module 'lazy.types'
 ---@type LazySpec
 return {
-	"sainnhe/gruvbox-material",
+	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
 	config = function()
-		-- Optionally configure and load the colorscheme
-		-- directly inside the plugin declaration.
-		vim.g.gruvbox_material_background = 'hard'
-		vim.g.gruvbox_material_enable_italic = true
-		vim.cmd.colorscheme("gruvbox-material")
+		vim.o.termguicolors = true
+		vim.cmd.colorscheme("tokyonight-night")
 	end,
 }
